3fb25ddbf4db8768bd86a94e9552cd58b5ebbe64
Simranjeet Singh <42518706+Sjsingh101@users.noreply.github.com>
2020-03-13 02:54:00 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 echo Hello bitch
-echo see my name ${GITHUB_USER}
+echo see my name $GITHUB_USER
 apt-get update
-echo ${{ github.repository }}
-echo ${{ secrets.URL }}
 #apt install wget -y
 #wget -r ${{ github.repository }}
 #apt-get install iputils-ping -y
