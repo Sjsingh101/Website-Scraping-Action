a7a8d7da3461e801cc58fac8197cd18a9ae40814
Simranjeet Singh <42518706+Sjsingh101@users.noreply.github.com>
2020-03-13 09:02:37 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,9 @@ apt install wget curl git -y
 curl -H "Authorization: token $OAUTH_TOKEN" https://api.github.com/user/repos -d '{"name":"Website","private":false}'
 wget -r $URL
 cd $URL
-rm -r .git
 git init
-git add .
-pwd
-git status
-git commit -m "Final HIt"
-git status
+git add --all
 git commit -m "Final"
+git branch -a
 git status
-git push https://$OAUTH_TOKEN:x-oauth-basic@github.com/$USER/Website.git master --force
+git push -u https://$OAUTH_TOKEN:x-oauth-basic@github.com/$USER/Website.git master --force
