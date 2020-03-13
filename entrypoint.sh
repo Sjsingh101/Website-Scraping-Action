158eb0e08cc2fe535e1d00b1695b771e71e0db2a
Simranjeet Singh <42518706+Sjsingh101@users.noreply.github.com>
2020-03-13 06:14:23 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,9 @@ echo Hello bitch
 apt-get update
 apt install wget curl git -y
 wget -r $URL
-echo try to change path
 cd $URL
-pwd
 rm -r .git
 curl -H "Authorization: token $OAUTH_TOKEN" https://api.github.com/user/repos -d '{"name":"Website","private":false}'
-ls -la
-cd $URL
-echo new path of website
-pwd
 git init
 git add .
 git status
