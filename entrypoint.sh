1e40870a256746bf8741a9fec396ebdb8644454a
Simranjeet Singh <42518706+Sjsingh101@users.noreply.github.com>
2020-03-13 09:25:56 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ wget -r $URL
 cd $URL
 rm -r .git
 git init
+git config --global user.email "sjsingh9090@gmail.com"
+git config --global user.name "$USER"
 git add .
 git status
 git commit -m "Final HIt"
