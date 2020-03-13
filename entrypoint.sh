362c3fbcf51ec362c4197fd8f2ed413c34ad2770
Simranjeet Singh <42518706+Sjsingh101@users.noreply.github.com>
2020-03-13 05:33:13 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,14 @@ apt-get update
 apt install wget curl git -y
 wget -r $URL
 cd $URL
-curl -H "Authorization: $OAUTH_TOKEN" https://api.github.com/user/repos -d '{"name":"Website","private":false}'
+curl -H "Authorization: token $OAUTH_TOKEN" https://api.github.com/user/repos -d '{"name":"Website","private":false}'
 git init
 git add .
+git status
 git commit -m "Initial Commit"
-git remote add test https://github.com/$USER/Website.git
-git config user.email "$EMAIL"
-git config user.name "$USER"
-git push test master
+git push https://$OAUTH_TOKEN:x-oauth-basic@github.com/$USER/Website.git master
+#git remote add test https://github.com/$USER/Website.git
+#git config user.email "$EMAIL"
+#git config user.name "$USER"
+#git push test master
 
