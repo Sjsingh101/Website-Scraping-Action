8feaa074e44094a07a92b87c1ad3d44ff0590be6
Simranjeet Singh <42518706+Sjsingh101@users.noreply.github.com>
2020-03-13 08:44:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-echo 'Website Scrapping Started At : `date`'
+ #!/bin/bash
+
+echo Hello bitch
 apt-get update
 apt install wget curl git -y
 curl -H "Authorization: token $OAUTH_TOKEN" https://api.github.com/user/repos -d '{"name":"Website","private":false}'
@@ -9,7 +10,6 @@ rm -r .git
 git init
 git add .
 git status
-git commit -m "Scrapped Website Uploaded"
+git commit -m "Final HIt"
 git status
 git push https://$OAUTH_TOKEN:x-oauth-basic@github.com/$USER/Website.git master --force
-
